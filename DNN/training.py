fbd122776d47974403f024d49fe91b16bc79307d
jrotter2 <john.rotter@ufl.edu>
2023-06-25 02:17:54 +0200

    DNN

--- DNN/training.py
+++ DNN/training.py
@@ -1,17 +1,26 @@
 import uproot
 import pandas
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
-from keras.utils import np_utils
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
+import json
+import numpy as np
+#from keras.models import Sequential
+#from keras.layers import Dense
+#from keras.wrappers.scikit_learn import KerasClassifier
+#from keras.utils import np_utils
+#from sklearn.model_selection import cross_val_score
+#from sklearn.model_selection import KFold
+#from sklearn.preprocessing import LabelEncoder
+#from sklearn.pipeline import Pipeline
 
 
-CATEGORIES = ["VBF", "ggH", "top", "WW", "DY"] #Titles for categories
-CAT_CONFIG_IDS = ["VBF", "ggH", "top", "WW", "DY"] #For now are the same as titles but may include versioning later
+MAX_EVENTS_PER_CATEGORY = 1000
+
+CATEGORIES = ["VBF", "ggH", "top", "WW"] #Titles for categories
+CAT_CONFIG_IDS = ["VBF", "ggH", "top", "WW"] #For now are the same as titles but may include versioning later
+
+CAT_CONFIG_IDS = ["VBF_2018v7", "ggH_2018v7", "top_2018v7", "WW_2018v7"]
+
+
+INPUT_VARS = ["mll","dphill","detall","ptll","drll","pt1","pt2","mth","mjj","detajj","dphijj","PuppiMET_pt","dphillmet","mcollWW"]
 
 CONFIG_FILE_PATH = "dnn_config.json"
 CONFIG_FILE = open(CONFIG_FILE_PATH, "r")
@@ -19,33 +28,57 @@ CONFIG_FILE_CONTENTS = CONFIG_FILE.read()
 CONFIG = json.loads(CONFIG_FILE_CONTENTS)
 
 def loadVariables():
-    X = []
-    Y = []
+    print("Started Loading Variables...")
+    X = {}
     for category in CAT_CONFIG_IDS:
-        for f in CONFIG[category]["files"]:
-            rootFile = uproot.open(f)
+        print("Category: " + category)
+        X[category] = []
+        basepath = CONFIG[category]["basepath"]
 
-            y_evt = [category == c for c in CAT_CONFIG_IDS] # will produce one-hot encoding
-            x_evt = []
-            ###
-            ### CONSTRUCT AND BUILD INPUT VARIABLES HERE
-            ###
-            X.append(x_evt)
-            Y.append(y_evt)
+        max_events_per_subcategory = MAX_EVENTS_PER_CATEGORY / len(CONFIG[category]["subpaths"])
+        for subcat_i in range(0,len(CONFIG[category]["subpaths"])):
+            weights  = CONFIG[category]["subpaths"][subcat_i]["weights"]
+            custom_weights  = CONFIG[category]["subpaths"][subcat_i]["custom_weights"]
+            files = CONFIG[category]["subpaths"][subcat_i]["files"]
+            current_nEvents_per_subcategory = 0
+            print(">Subcategory: " + CONFIG[category]["subpaths"][subcat_i]["name"] + " | Allowed # Events: " + str(max_events_per_subcategory)) 
 
+            for f in files:
+                print("    Loading Next File... Currently at Event " + str(current_nEvents_per_subcategory) + " of " + str(max_events_per_subcategory))
+                if(max_events_per_subcategory - current_nEvents_per_subcategory <= 0):
+                    break
+                rootFile = uproot.open(basepath + f)
+
+                nEvents = rootFile["Events"]._fEntries
+                nEvents_for_file = min(nEvents, max_events_per_subcategory - current_nEvents_per_subcategory)           
+
+                full_wgts = np.ones(nEvents_for_file)
+                #for wgt in weights:
+                #    full_wgts = np.multiply(rootFile["Events/" + wgt].array()[:nEvents_for_file], full_wgt)
+
+                ## ADD CUSTOM WEIGHTS               
+                input_vars = [np.multiply(rootFile["Events/" + var].array()[:nEvents_for_file], full_wgts) for var in INPUT_VARS]
+                x_evt = np.transpose(input_vars)
+                for arr in x_evt:
+                    X[category].append(arr)
+
+                current_nEvents_per_subcategory = current_nEvents_per_subcategory + nEvents_for_file
+    return X
 def baseline_model():
     model = Sequential()
-    model.add(Dense(240, input_dim=26, activation='relu'))
-    model.add(Dense(120, activation='relu'))
-    model.add(Dense(60, activation='relu'))
+    model.add(Dense(20, input_dim=len(INPUT_VARS), activation='relu'))
+    model.add(Dense(15, activation='relu'))
+    model.add(Dense(10, activation='relu'))
     model.add(Dense(4, activation='softmax'))
     # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-X, Y = loadVariables()
-estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=10, verbose=0)
-kfold = KFold(n_splits=10, shuffle=True)
-results = cross_val_score(estimator, X, Y, cv=kfold)
-print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+X = loadVariables()
+print(len(X["WW_2018v7"]))
+#estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=10, verbose=0)
+#kfold = KFold(n_splits=10, shuffle=True)
+#results = cross_val_score(estimator, X, Y, cv=kfold)
+#print(results)
+#print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 
